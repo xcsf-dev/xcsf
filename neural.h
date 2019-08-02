9e3787a4d6a3367884e69efe743b6dfc1c79ae46
Richard Preen <rpreen@gmail.com>
2019-08-02 21:34:40 +0100

    update year

--- neural.h
+++ neural.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012--2015 Richard Preen <rpreen@gmail.com>
+ * Copyright (C) 2012--2019 Richard Preen <rpreen@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
