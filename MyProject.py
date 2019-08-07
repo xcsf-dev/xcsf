829124b80e4901e1465ed3d96babf88d8a3972c6
Richard Preen <rpreen@gmail.com>
2019-08-07 14:16:25 +0100

    update python example

--- MyProject.py
+++ MyProject.py
@@ -6,14 +6,16 @@ from sklearn.preprocessing import normalize
 
 iris = datasets.load_iris()
 train_X, train_Y = iris.data, iris.target
+
 train_X = normalize(train_X)
 train_Y = normalize(train_Y.reshape(train_Y.shape[0],-1), norm='max', axis=0)
 
 print("train_X shape = "+str(np.shape(train_X)))
 print("train_Y shape = "+str(np.shape(train_Y)))
 
-num_input_vars = len(train_X[0])
-num_output_vars = 1
+num_input_vars = np.shape(train_X)[1]
+num_output_vars = np.shape(train_Y)[1] 
+
 print("xvars = "+str(num_input_vars) + " yvars = " + str(num_output_vars))
 
 # initialise XCSF
