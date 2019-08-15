11c432ff29239a6c1ebf7bfe911251e3b0dfe0cd
Richard Preen <rpreen@gmail.com>
2019-08-15 12:21:42 +0100

    remove whitespace

--- MyProject.py
+++ MyProject.py
@@ -104,8 +104,7 @@ lm_mse = mean_squared_error(lm_pred, test_Y)
 print('Linear regression MSE = %.4f' % (lm_mse))
 
 # compare with MLP regressor
-mlp = MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam', learning_rate='adaptive',
-        max_iter=1000, learning_rate_init=0.01, alpha=0.01) 
+mlp = MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam', learning_rate='adaptive', max_iter=1000, learning_rate_init=0.01, alpha=0.01)
 mlp.fit(train_X, train_Y.ravel())
 mlp_pred = mlp.predict(test_X)
 mlp_mse = mean_squared_error(mlp_pred, test_Y)
