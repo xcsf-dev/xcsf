769fa963b3503dc0a125b9c52ac1a03be01ff834
Richard Preen <rpreen@gmail.com>
2019-08-16 16:42:11 +0100

    revert momentum change

--- xcsf/neural.c
+++ xcsf/neural.c
@@ -226,19 +226,14 @@ double neuron_propagate(XCSF *xcsf, NEURON *n, double *input)
 
 void neuron_learn(XCSF *xcsf, NEURON *n, double error)
 {
-    for(int i = 0; i < n->num_inputs; i++) {
-        n->v[i] = (xcsf->MOMENTUM * n->v[i]) + (error * n->input[i] * xcsf->XCSF_ETA);
-        n->weights[i] += n->v[i];
-        if(xcsf->NESTEROV) {
-            n->weights[i] += xcsf->MOMENTUM * n->v[i];
-        }
-    }
-    n->v[n->num_inputs] = (xcsf->MOMENTUM * n->v[n->num_inputs])
-        + (error * n->input[n->num_inputs] * xcsf->XCSF_ETA);
-    n->weights[n->num_inputs] += n->v[n->num_inputs];
-    if(xcsf->NESTEROV) {
-        n->weights[n->num_inputs] += xcsf->MOMENTUM * n->v[n->num_inputs];
-    }
+	for(int i = 0; i < n->num_inputs; i++) {
+		n->weights[i] += xcsf->MOMENTUM * n->v[i];
+		n->v[i] = error * n->input[i] * xcsf->XCSF_ETA;
+		n->weights[i] += n->v[i];
+	}
+	n->weights[n->num_inputs] += xcsf->MOMENTUM * n->v[n->num_inputs];
+	n->v[n->num_inputs] = error * xcsf->XCSF_ETA;
+	n->weights[n->num_inputs] += n->v[n->num_inputs];
 }
 
 void neuron_set_activation(XCSF *xcsf, NEURON *n, int func)
