ee34d303b371e420151a99b0db345031f28b6655
Richard Preen <rpreen@gmail.com>
2019-11-25 23:05:52 +0000

    comments

--- xcsf/env_csv.c
+++ xcsf/env_csv.c
@@ -31,16 +31,24 @@
 #include "utils.h"
 #include "env_csv.h"
 
-#define MAX_DATA 100000
-#define MAX_LINE_LENGTH 200
-#define DELIM ","
+#define MAX_ROWS 100000 //!< maximum number of instances
+#define MAX_COLS 200 //!< maximum line length
+#define MAX_NAME 200 //!< maximum file name length
+#define DELIM "," //!< file delimiter
 
 void env_csv_read(char *fname, double **data, int *num_prob, int *num_vars);
 
+/**
+ * @brief Parses specified csv files into training and testing data sets.
+ * @param infile The base name of the csv files to read.
+ * @param train_data The data structure to load the training data.
+ * @param test_data The data structure to load the testing data.
+ *
+ * @details Expects an identical number of x and y rows.
+ */
 void env_csv_input_read(char *infile, INPUT *train_data, INPUT *test_data)
 {
-    // expects an identical number of x and y rows
-    char name[200];
+    char name[MAX_NAME];
     sprintf(name, "%s_train_x.csv", infile);
     env_csv_read(name, &train_data->x, &train_data->rows, &train_data->x_cols);
     sprintf(name, "%s_train_y.csv", infile);
@@ -51,9 +59,17 @@ void env_csv_input_read(char *infile, INPUT *train_data, INPUT *test_data)
     env_csv_read(name, &test_data->y, &test_data->rows, &test_data->y_cols);
 }
 
+/**
+ * @brief Parses a specified csv file into training and testing data sets.
+ * @param fname The name of the csv file to read.
+ * @param data The data structure to load the data (set by this function).
+ * @param num_rows The number of rows in the dataset (set by this function).
+ * @param num_cols The number of columns in the dataset (set by this function).
+ *
+ * @details Provided a file name will set the data, num_rows, and num_cols.
+ */
 void env_csv_read(char *fname, double **data, int *num_rows, int *num_cols)
 {
-    // Provided a file name: will set the data, num_rows, num_cols 
     FILE *fin = fopen(fname, "rt");
     if(fin == 0) {
         printf("Error opening file: %s. %s.\n", fname, strerror(errno));
@@ -62,29 +78,28 @@ void env_csv_read(char *fname, double **data, int *num_rows, int *num_cols)
     // ascertain the file length and number of variables per line
     *num_rows = 0;
     *num_cols = 0;
-    char line[MAX_LINE_LENGTH];
-    while(fgets(line, MAX_LINE_LENGTH, fin) != NULL) {
-        if(*num_rows > MAX_DATA) {
-            printf("data file %s is too big; maximum: %d\n", fname, MAX_DATA);
+    char line[MAX_COLS];
+    while(fgets(line, MAX_COLS, fin) != NULL) {
+        if(*num_rows > MAX_ROWS) {
+            printf("data file %s is too big; maximum: %d\n", fname, MAX_ROWS);
             exit(EXIT_FAILURE);
         }        
         // use the first line to count the number of variables on a line
         if(*num_rows == 0) {
             char *ptok = strtok(line, DELIM);
             while(ptok != NULL) {
-                if(strnlen(ptok,MAX_LINE_LENGTH) > 0) {
+                if(strnlen(ptok,MAX_COLS) > 0) {
                     (*num_cols)++;
                 }
                 ptok = strtok(NULL, DELIM);
             }
         }
-        // count number of lines
-        (*num_rows)++;
+        (*num_rows)++; // count the number of lines
     }
     // read data file to memory
     rewind(fin);
     *data = malloc(sizeof(double) * (*num_cols) * (*num_rows));
-    for(int i = 0; fgets(line,MAX_LINE_LENGTH,fin) != NULL; i++) {
+    for(int i = 0; fgets(line,MAX_COLS,fin) != NULL; i++) {
         (*data)[i * (*num_cols)] = atof(strtok(line, DELIM));
         for(int j = 1; j < *num_cols; j++) {
             (*data)[i * (*num_cols)+j] = atof(strtok(NULL, DELIM));
@@ -94,6 +109,10 @@ void env_csv_read(char *fname, double **data, int *num_rows, int *num_cols)
     printf("Loaded: %s: %d rows, %d cols\n", fname, *num_rows, *num_cols);
 }
 
+/**
+ * @brief Frees an input data structure.
+ * @param data The input data structure to free.
+ */
 void env_csv_input_free(INPUT *data)
 {                 
     free(data->x);
