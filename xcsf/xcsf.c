ecdeaf4afb8902fbcf9417dfe13dba4f018603f4
Richard Preen <rpreen@gmail.com>
2020-01-31 15:45:30 +0000

    clean up

--- xcsf/xcsf.c
+++ xcsf/xcsf.c
@@ -52,8 +52,7 @@ static size_t xcsf_save_params(const XCSF *xcsf, FILE *fp);
  */
 double xcsf_fit1(XCSF *xcsf, const INPUT *train_data, _Bool shuffle)
 {  
-    const INPUT *test_data = NULL;
-    return xcsf_fit2(xcsf, train_data, test_data, shuffle);
+    return xcsf_fit2(xcsf, train_data, NULL, shuffle);
 }
 
 /**
