373d42657c8a86c65d3bf81d4102340f309baf84
Richard Preen <rpreen@gmail.com>
2020-02-20 12:11:03 +0000

    add gpu activations

---
+++ xcsf/neural_activation_kernels.h
@@ -0,0 +1,29 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+                      
+/**
+ * @file neural_activation_kernels.cu
+ * @author Richard Preen <rpreen@gmail.com>
+ * @copyright The Authors.
+ * @date 2020.
+ * @brief CUDA neural network activation functions.
+ */ 
+ 
+#pragma once
+
+#include <cuda_runtime.h>
+
+void activate_array_gpu(double *x, int n, int a, const cudaStream_t *stream);
+void gradient_array_gpu(double *x, int n, int a, double *delta, const cudaStream_t *stream);
