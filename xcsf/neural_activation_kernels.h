05c39e2c330cf30c96406205bae4fadd896cc0fe
Richard Preen <rpreen@gmail.com>
2020-02-26 09:16:01 +0000

    whitespace

--- xcsf/neural_activation_kernels.h
+++ xcsf/neural_activation_kernels.h
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-                      
+
 /**
  * @file neural_activation_kernels.h
  * @author Richard Preen <rpreen@gmail.com>
