73cf66b2523fbcd437610862cee6080b32b5dcac
Richard Preen <rpreen@gmail.com>
2020-08-12 18:42:59 +0100

    input representation

--- xcsf/neural_layer_connected.c
+++ xcsf/neural_layer_connected.c
@@ -223,7 +223,9 @@ neural_layer_connected_resize(const struct XCSF *xcsf, struct LAYER *l,
     l->n_weights = n_weights;
     l->n_inputs = prev->n_outputs;
     layer_calc_n_active(l);
-    layer_ensure_input_represention(l);
+    if (l->options & LAYER_EVOLVE_CONNECT) {
+        layer_ensure_input_represention(l);
+    }
 }
 
 _Bool
