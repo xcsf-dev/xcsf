246a002542f2a4e16db9f72e852968fac199d5a2
Richard Preen <rpreen@gmail.com>
2020-09-09 18:25:07 +0100

    update example

--- python/example_cartpole.py
+++ python/example_cartpole.py
@@ -21,6 +21,8 @@ This example demonstrates the XCSF multi-step reinforcement learning mechanisms
 to solve the cart-pole problem from the OpenAI Gym.
 """
 
+import random
+from collections import deque
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib import animation
@@ -93,8 +95,8 @@ xcs.ACT_TYPE = 0 # integer actions
 xcs.PRED_TYPE = 5 # neural network predictions
 xcs.PRED_OUTPUT_ACTIVATION = 3 # linear
 xcs.PRED_HIDDEN_ACTIVATION = 9 # selu
-xcs.PRED_NUM_NEURONS = [5] # initial neurons
-xcs.PRED_MAX_NEURONS = [5] # maximum neurons
+xcs.PRED_NUM_NEURONS = [10] # initial neurons
+xcs.PRED_MAX_NEURONS = [10] # maximum neurons
 xcs.PRED_EVOLVE_WEIGHTS = False
 xcs.PRED_EVOLVE_NEURONS = False
 xcs.PRED_EVOLVE_FUNCTIONS = False
@@ -110,16 +112,36 @@ xcs.print_params()
 # Execute experiment
 #####################
 
+MIN_EXPLORE = 0.2 # the minimum exploration rate
+EXPLORE_DECAY = 0.995 # the decay of exploration after each batch replay
 N = 100 # maximum 10,000 learning trials/episodes
 trials = np.zeros(N)
-score = np.zeros(N)
 error = np.zeros(N)
+memory = deque(maxlen = 200 * N)
+scores = deque(maxlen = N)
 
 frames = [] # for creating a gif
 fscore = []
 ftrial = []
 
-# learning trials/episodes
+def replay(replay_size = 200):
+    """Performs experience replay updates"""
+    batch = random.sample(memory, min(len(memory), replay_size))
+    for state, action, reward, next_state, is_reset in batch:
+        y_target = reward
+        if not is_reset: # predict next state payoff
+            prediction_array = xcs.predict(next_state.reshape(1, X_DIM))[0]
+            y_target += xcs.GAMMA * np.max(prediction_array)
+        xcs.init_trial()
+        xcs.init_step()
+        xcs.decision(state, True) # create match set and forward prop state
+        xcs.update(y_target, True, action) # create action set and update
+        xcs.end_step()
+        xcs.end_trial()
+    if xcs.P_EXPLORE > MIN_EXPLORE: # increasingly exploit
+        xcs.P_EXPLORE *= EXPLORE_DECAY
+
+# learning episodes
 for i in range(N):
     for j in range(xcs.PERF_TRIALS):
         # explore trial/episode
@@ -129,12 +151,13 @@ for i in range(N):
             xcs.init_step()
             action = xcs.decision(state, True)
             next_state, reward, is_reset, info = env.step(action)
-            xcs.update(reward, is_reset)
             xcs.end_step()
+            memory.append((state, action, reward, next_state, is_reset))
             state = next_state
             if is_reset:
                 break
         xcs.end_trial()
+        replay() # perform experience replay update
         # exploit trial/episode
         episode_score = 0
         cnt = 0
@@ -162,16 +185,17 @@ for i in range(N):
                         ftrial.append(ftrial[-1])
                 break
         xcs.end_trial()
-        score[i] += episode_score
+        scores.append(episode_score)
         error[i] += err / float(cnt)
-    score[i] /= float(xcs.PERF_TRIALS)
     error[i] /= float(xcs.PERF_TRIALS)
     trials[i] = (i + 1) * xcs.PERF_TRIALS
+    mean_score = np.mean(scores)
     status = ("episodes=%d score=%.2f error=%.5f" %
-              (trials[i], score[i], error[i]))
+              (trials[i], mean_score, error[i]))
     print(status)
-    if score[i] > env.spec.reward_threshold: # solved
-        print("solved: score %.2f > %.2f" % (score[i], env.spec.reward_threshold))
+    if mean_score > env.spec.reward_threshold: # solved
+        print("solved: score %.2f > %.2f" %
+              (mean_score, env.spec.reward_threshold))
         break
 
 # final exploit trial/episode
