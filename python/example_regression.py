eb25af377e3b67dde2ed9dafc10d8111f1080692
Richard Preen <rpreen@gmail.com>
2020-09-28 13:53:07 +0100

    :chart_with_downwards_trend: update regression example

--- python/example_regression.py
+++ python/example_regression.py
@@ -16,10 +16,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-"""This example demonstrates the XCSF supervised learning mechanisms to perform
+"""
+This example demonstrates the XCSF supervised learning mechanisms to perform
 regression on the Boston house price dataset. Classifiers are composed of
 neural network conditions and predictions. A single dummy action is performed
-such that [A] = [M]."""
+such that [A] = [M].
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -58,15 +60,24 @@ X_test = scaler.fit_transform(X_test)
 y_train = minmax_scale(y_train, feature_range=(0, 1))
 y_test = minmax_scale(y_test, feature_range=(0, 1))
 
-print("X_train shape = "+str(np.shape(X_train)))
-print("y_train shape = "+str(np.shape(y_train)))
-print("X_test shape = "+str(np.shape(X_test)))
-print("y_test shape = "+str(np.shape(y_test)))
-
 # get number of input and output variables
 X_DIM = np.shape(X_train)[1]
 Y_DIM = np.shape(y_train)[1]
-print("x_dim = "+str(X_DIM) + ", y_dim = " + str(Y_DIM))
+print('x_dim = '+str(X_DIM) + ', y_dim = ' + str(Y_DIM))
+
+# 10% of training for validation
+N_VAL = int(len(X_train) * 0.1)
+X_val = X_train[:N_VAL]
+y_val = y_train[:N_VAL]
+X_train = X_train[N_VAL:]
+y_train = y_train[N_VAL:]
+
+print('X_train shape = ' + str(np.shape(X_train)))
+print('y_train shape = ' + str(np.shape(y_train)))
+print('X_val shape = ' + str(np.shape(X_val)))
+print('y_val shape = ' + str(np.shape(y_val)))
+print('X_test shape = ' + str(np.shape(X_test)))
+print('y_test shape = ' + str(np.shape(y_test)))
 
 ###################
 # Initialise XCSF
@@ -80,7 +91,7 @@ xcs.OMP_NUM_THREADS = 8
 xcs.POP_SIZE = 500
 xcs.MAX_TRIALS = 1000 # number of trials per fit()
 xcs.LOSS_FUNC = 1 # MSE
-xcs.EPS_0 = 0.004 # target error
+xcs.EPS_0 = 0.005 # target error
 xcs.ALPHA = 1 # accuracy offset
 xcs.NU = 20 # accuracy slope
 xcs.THETA_EA = 50 # EA invocation frequency
@@ -91,8 +102,8 @@ xcs.MAX_NEURON_GROW = 1 # max neurons to add/remove per mutation event
 xcs.COND_TYPE = 3 # neural network conditions
 xcs.COND_OUTPUT_ACTIVATION = 3 # linear
 xcs.COND_HIDDEN_ACTIVATION = 1 # relu
-xcs.COND_NUM_NEURONS = [1]
-xcs.COND_MAX_NEURONS = [20]
+xcs.COND_NUM_NEURONS = [10]
+xcs.COND_MAX_NEURONS = [100]
 xcs.COND_EVOLVE_WEIGHTS = True
 xcs.COND_EVOLVE_NEURONS = True
 xcs.COND_EVOLVE_FUNCTIONS = False
@@ -101,15 +112,15 @@ xcs.COND_EVOLVE_CONNECTIVITY = True
 xcs.PRED_TYPE = 5 # neural network predictors
 xcs.PRED_OUTPUT_ACTIVATION = 7 # softplus
 xcs.PRED_HIDDEN_ACTIVATION = 1 # relu
-xcs.PRED_NUM_NEURONS = [50]
-xcs.PRED_MAX_NEURONS = [50]
+xcs.PRED_NUM_NEURONS = [10]
+xcs.PRED_MAX_NEURONS = [10]
 xcs.PRED_EVOLVE_WEIGHTS = True
 xcs.PRED_EVOLVE_NEURONS = False
 xcs.PRED_EVOLVE_FUNCTIONS = False
 xcs.PRED_EVOLVE_CONNECTIVITY = True
 xcs.PRED_EVOLVE_ETA = True
 xcs.PRED_SGD_WEIGHTS = True
-xcs.PRED_ETA = 0.01 # maximum gradient descent rate
+xcs.PRED_ETA = 0.1 # maximum gradient descent rate
 
 xcs.print_params()
 
@@ -117,12 +128,17 @@ xcs.print_params()
 # Example plotting in matplotlib
 ##################################
 
-N = 100 # 100,000 trials
+N = 200 # 200,000 trials
 trials = np.zeros(N)
 psize = np.zeros(N)
 msize = np.zeros(N)
 train_mse = np.zeros(N)
-test_mse = np.zeros(N)
+val_mse = np.zeros(N)
+
+VAL_PERIOD = 10 # validation mean moving average length
+val_min = 999999 # minimum validation error
+val_trial = 0 # trial number the system was checkpointed
+
 bar = tqdm(total=N) # progress bar
 for i in range(N):
     # train
@@ -130,11 +146,17 @@ for i in range(N):
     trials[i] = xcs.time() # number of trials so far
     psize[i] = xcs.pop_size() # current population size
     msize[i] = xcs.msetsize() # avg match set size
-    # test
-    test_mse[i] = xcs.score(X_test, y_test)
+    # validate
+    val_mse[i] = xcs.score(X_val, y_val)
+    if i > VAL_PERIOD: # simple moving average of mean validation error
+        val_mean = np.mean(val_mse[i - VAL_PERIOD : i])
+        if val_mean < val_min: # checkpoint lowest validation error
+            xcs.store()
+            val_min = val_mean
+            val_trial = trials[i]
     # update status
-    status = ("trials=%d train_mse=%.5f test_mse=%.5f psize=%d msize=%.1f" %
-              (trials[i], train_mse[i], test_mse[i], psize[i], msize[i]))
+    status = ('trials=%d train_mse=%.5f val_mse=%.5f psize=%d msize=%.1f' %
+              (trials[i], train_mse[i], val_mse[i], psize[i], msize[i]))
     bar.set_description(status)
     bar.refresh()
     bar.update(1)
@@ -143,7 +165,7 @@ bar.close()
 # plot XCSF learning performance
 plt.figure(figsize=(10, 6))
 plt.plot(trials, train_mse, label='Train MSE')
-plt.plot(trials, test_mse, label='Test MSE')
+plt.plot(trials, val_mse, label='Validation MSE')
 plt.grid(linestyle='dotted', linewidth=1)
 plt.axhline(y=xcs.EPS_0, xmin=0, xmax=1, linestyle='dashed', color='k')
 plt.title('XCSF Training Performance', fontsize=14)
@@ -158,25 +180,30 @@ plt.show()
 ############################
 
 # final XCSF test score
+print("*****************************")
+print('Restoring system from trial %d with val_mse=%.5f' % (val_trial, val_min))
+xcs.retrieve()
 xcsf_pred = xcs.predict(X_test)
 xcsf_mse = mean_squared_error(xcsf_pred, y_test)
-print('XCSF MSE = %.4f' % (xcsf_mse))
+print('XCSF Test MSE = %.4f' % (xcsf_mse))
 
 # compare with linear regression
 lm = LinearRegression()
 lm.fit(X_train, y_train)
 lm_pred = lm.predict(X_test)
 lm_mse = mean_squared_error(lm_pred, y_test)
-print('Linear regression MSE = %.4f' % (lm_mse))
+print('Linear regression Test MSE = %.4f' % (lm_mse))
 
 # compare with MLP regressor
-mlp = MLPRegressor(hidden_layer_sizes=(50,), activation='relu', solver='adam',
+X_train = np.vstack((X_train, X_val))
+y_train = np.vstack((y_train, y_val))
+mlp = MLPRegressor(hidden_layer_sizes=(10,), activation='relu', solver='adam',
                    learning_rate='adaptive', learning_rate_init=0.01,
-                   max_iter=1000, alpha=0.01)
+                   max_iter=1000, alpha=0.01, validation_fraction=0.1)
 mlp.fit(X_train, y_train.ravel())
 mlp_pred = mlp.predict(X_test)
 mlp_mse = mean_squared_error(mlp_pred, y_test)
-print('MLP Regressor MSE = %.4f' % (mlp_mse))
+print('MLP Regressor Test MSE = %.4f' % (mlp_mse))
 
 #####################################
 # Show some predictions vs. answers
