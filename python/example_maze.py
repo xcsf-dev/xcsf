e0861bacbd0d7998902cc022b2ad76e7c6cf51ab
Richard Preen <rpreen@gmail.com>
2020-10-03 13:37:56 +0100

    :art: refactor

--- python/example_maze.py
+++ python/example_maze.py
@@ -19,33 +19,6 @@
 """
 This example demonstrates the XCSF multi-step reinforcement learning mechanisms
 to solve discrete mazes loaded from a specified input file.
-
-The maze class reads in the chosen maze from a file where each entry specifies
-a distinct position in the maze. The maze is toroidal and if the agent/animat
-reaches one edge it can reenter the maze from the other side. Obstacles are
-coded as 'O' and 'Q', empty positions as '*', and food as 'F' or 'G'. The 8
-adjacent cells are perceived by the animat and 8 movements are possible to one
-of the adjacent cells (if not blocked.) The animat is initially placed at a
-random empty position. The goal is to find the shortest path to the food.
-
-Some mazes require a form of memory to be solved optimally.
-The optimal average number of steps for each maze is:
-
-woods1: 1.7
-woods2: 1.7
-woods14: 9.5
-maze4: 3.5
-maze5: 4.61
-maze6: 5.19
-maze7: 4.33
-maze10: 5.11
-woods101: 2.9
-woods101half: 3.1
-woods102: 3.31
-mazef1: 1.8
-mazef2: 2.5
-mazef3: 3.375
-mazef4: 4.5
 """
 
 import os
@@ -58,13 +31,32 @@ from tqdm import tqdm
 import xcsf.xcsf as xcsf
 
 class Maze:
-    """Maze problem environment."""
+    """
+    Maze problem environment.
 
-    def __init__(self, file_name):
-        """Constructs a new maze problem from the maze file_name."""
+    The maze class reads in the chosen maze from a file where each entry specifies
+    a distinct position in the maze. The maze is toroidal and if the agent/animat
+    reaches one edge it can reenter the maze from the other side. Obstacles are
+    coded as 'O' and 'Q', empty positions as '*', and food as 'F' or 'G'. The 8
+    adjacent cells are perceived by the animat and 8 movements are possible to one
+    of the adjacent cells (if not blocked.) The animat is initially placed at a
+    random empty position. The goal is to find the shortest path to the food.
+
+    Some mazes require a form of memory to be solved optimally.
+    """
+
+    optimal = { 'woods1': 1.7, 'woods2': 1.7, 'woods14': 9.5, 'maze4': 3.5,
+            'maze5': 4.61, 'maze6': 5.19, 'maze7': 4.33, 'maze10': 5.11,
+            'woods101': 2.9, 'woods101half': 3.1, 'woods102': 3.31,
+            'mazef1': 1.8, 'mazef2': 2.5, 'mazef3': 3.375, 'mazef4': 4.5 }
+    max_payoff = 1 #: reward for finding the goal
+
+    def __init__(self, filename):
+        """ Constructs a new maze problem given a maze file name. """
+        self.name = filename
         self.maze = [] #: maze as read from the input file
         line = []
-        path = os.path.normpath("../env/maze/"+file_name+".txt")
+        path = os.path.normpath('../env/maze/'+filename+'.txt')
         with open(path) as f:
             while True:
                 c = f.read(1)
@@ -80,12 +72,9 @@ class Maze:
         self.state = np.zeros(8) #: current maze state
         self.x_pos = 0 #: current x position within the maze
         self.y_pos = 0 #: current y position within the maze
-        self.done = False #: whether the goal state has been reached
-        self.max_payoff = 1 #: reward for finding the goal
 
     def reset(self):
-        """Resets a maze problem: generating a new random start position."""
-        self.done = False
+        """ Resets a maze problem: generating a new random start position. """
         while True:
             self.x_pos = random.randint(0, self.x_size - 1)
             self.y_pos = random.randint(0, self.y_size - 1)
@@ -93,7 +82,7 @@ class Maze:
                 break
 
     def sensor(self, x_pos, y_pos):
-        """Returns the real-number representation of a discrete maze cell."""
+        """ Returns the real-number representation of a discrete maze cell. """
         s = self.maze[y_pos][x_pos]
         if s == '*':
             return 0.1
@@ -105,11 +94,11 @@ class Maze:
             return 0.7
         if s == 'Q':
             return 0.9
-        print("invalid maze state: "+str(s))
+        print('invalid maze state: '+str(s))
         sys.exit()
 
     def update_state(self):
-        """Sets the current state to a real-vector representing the sensory input."""
+        """ Sets the current state to a real-vector representing the sensory input. """
         spos = 0
         for y in range(-1, 2):
             for x in range(-1, 2):
@@ -121,9 +110,9 @@ class Maze:
                 spos += 1
 
     def execute(self, act):
-        """Executes an action within the maze and returns the immediate reward."""
+        """ Executes an action, returns reward and whether terminal state reached. """
         if act < 0 or act > 7:
-            print("invalid maze action")
+            print('invalid maze action')
             sys.exit()
         x_moves = [0, 1, 1, 1, 0, -1, -1, -1]
         y_moves = [-1, -1, 0, 1, 1, 1, 0, -1]
@@ -131,16 +120,19 @@ class Maze:
         y_new = ((self.y_pos + y_moves[act]) % self.y_size + self.y_size) % self.y_size
         s = self.maze[y_new][x_new]
         if s in ('O', 'Q'):
-            return 0
+            return 0, False
         self.x_pos = x_new
         self.y_pos = y_new
         if s == '*':
-            return 0
+            return 0, False
         if s in ('F', 'G'):
-            self.done = True
-            return self.max_payoff
-        print("invalid maze type")
+            return Maze.max_payoff, True
+        print('invalid maze type')
         sys.exit()
+
+    def optimal_steps(self):
+        """ Returns the optimal number of steps to the goal. """
+        return Maze.optimal[self.name]
 
 ###################
 # Initialise XCSF
@@ -183,9 +175,7 @@ steps = np.zeros(N)
 error = np.zeros(N)
 bar = tqdm(total=N) # progress bar
 
-MAZE_NAME = "maze4"
-GOAL_STEPS = 3.5
-maze = Maze(MAZE_NAME)
+maze = Maze('maze4')
 
 for i in range(N):
     for j in range(xcs.PERF_TRIALS):
@@ -196,10 +186,10 @@ for i in range(N):
             xcs.init_step()
             maze.update_state()
             action = xcs.decision(maze.state, True) # explore
-            reward = maze.execute(action)
-            xcs.update(reward, maze.done)
+            reward, done = maze.execute(action)
+            xcs.update(reward, done)
             xcs.end_step()
-            if maze.done:
+            if done:
                 break
         xcs.end_trial()
         # exploit trial
@@ -211,12 +201,12 @@ for i in range(N):
             xcs.init_step()
             maze.update_state()
             action = xcs.decision(maze.state, False) # exploit
-            reward = maze.execute(action)
-            xcs.update(reward, maze.done)
-            err += xcs.error(reward, maze.done, maze.max_payoff)
+            reward, done = maze.execute(action)
+            xcs.update(reward, done)
+            err += xcs.error(reward, done, Maze.max_payoff)
             cnt += 1
             xcs.end_step()
-            if maze.done:
+            if done:
                 break
         xcs.end_trial()
         steps[i] += cnt
@@ -227,7 +217,7 @@ for i in range(N):
     psize[i] = xcs.pop_size() # current population size
     msize[i] = xcs.msetsize() # avg match set size
     # update status
-    status = ("trials=%d steps=%.5f error=%.5f psize=%d msize=%.1f" %
+    status = ('trials=%d steps=%.5f error=%.5f psize=%d msize=%.1f' %
               (trials[i], steps[i], error[i], psize[i], msize[i]))
     bar.set_description(status)
     bar.refresh()
@@ -241,8 +231,8 @@ bar.close()
 plt.figure(figsize=(10, 6))
 plt.plot(trials, steps)
 plt.grid(linestyle='dotted', linewidth=1)
-plt.axhline(y=GOAL_STEPS, xmin=0, xmax=1, linestyle='dashed', color='k')
-plt.title(MAZE_NAME, fontsize=14)
+plt.axhline(y=maze.optimal_steps(), xmin=0, xmax=1, linestyle='dashed', color='k')
+plt.title(maze.name, fontsize=14)
 plt.ylabel('Steps to Goal', fontsize=12)
 plt.xlabel('Trials', fontsize=12)
 plt.xlim([0, N * xcs.PERF_TRIALS])
@@ -262,32 +252,32 @@ screen.setup(WIDTH + 4, HEIGHT + 8)
 screen.setworldcoordinates(0, 0, WIDTH, HEIGHT)
 
 def draw_maze(XOFF, YOFF):
-    """Draws the background and outline of the current maze."""
+    """ Draws the background and outline of the current maze. """
     bg = Turtle(visible=False)
     screen.tracer(False)
     bg.penup()
-    bg.shape("square")
+    bg.shape('square')
     bg.shapesize(1, 1)
     for y in range(maze.y_size):
         for x in range(maze.x_size):
             s = maze.maze[y][x]
             if s == '*':
-                bg.color("white")
+                bg.color('white')
             if s == 'O':
-                bg.color("black")
+                bg.color('black')
             if s == 'G':
-                bg.color("yellow")
+                bg.color('yellow')
             if s == 'F':
                 bg.color("yellow")
             if s == 'Q':
-                bg.color("brown")
+                bg.color('brown')
             bg.goto(XOFF + x * CELL_SIZE, YOFF + y * CELL_SIZE)
             bg.stamp()
     XOFF = XOFF - CELL_SIZE / 2
     YOFF = YOFF - CELL_SIZE / 2
     bg.goto(XOFF, YOFF)
     bg.pensize(2)
-    bg.color("black")
+    bg.color('black')
     bg.pendown()
     bg.goto(XOFF, YOFF + GRID_HEIGHT * CELL_SIZE)
     bg.goto(XOFF + GRID_WIDTH * CELL_SIZE, YOFF + GRID_HEIGHT * CELL_SIZE)
@@ -296,11 +286,11 @@ def draw_maze(XOFF, YOFF):
     bg.penup()
 
 def visualise(XOFF, YOFF):
-    """Executes an XCSF exploit run through the maze and draws the path."""
+    """ Executes an XCSF exploit run through the maze and draws the path. """
     agent = Turtle(visible=True)
-    agent.shape("turtle")
-    agent.color("green")
-    agent.speed("normal")
+    agent.shape('turtle')
+    agent.color('green')
+    agent.speed('normal')
     agent.shapesize(0.5, 0.5)
     agent.pensize(2)
     maze.reset()
@@ -314,11 +304,11 @@ def visualise(XOFF, YOFF):
             screen.tracer(True)
             agent.pendown()
         action = xcs.decision(maze.state, False)
-        reward = maze.execute(action)
+        reward, done = maze.execute(action)
         agent.goto(XOFF + maze.x_pos * CELL_SIZE, YOFF + maze.y_pos * CELL_SIZE)
-        xcs.update(reward, maze.done)
+        xcs.update(reward, done)
         xcs.end_step()
-        if maze.done:
+        if done:
             break
     xcs.end_trial()
 
@@ -333,4 +323,4 @@ for i in range(8):
         draw_maze(XOFF, YOFF)
         visualise(XOFF, YOFF)
 
-input("Press enter to exit.")
+input('Press enter to exit.')
