76587c1793ec2a50d1211211c1d6c212636934a2
Richard Preen <rpreen@gmail.com>
2021-12-04 10:54:02 +0000

    :new: increment version number

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ long_description = (this_directory / 'README.md').read_text()
 
 setup(
     name = 'xcsf',
-    version = '1.1.2',
+    version = '1.1.3',
     license = 'GPL-3.0',
     maintainer = 'Richard Preen',
     maintainer_email = 'rpreen@gmail.com',
