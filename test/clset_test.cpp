3442b8b002ceb85e683a93dc50afe2776198a207
Richard Preen <rpreen@gmail.com>
2023-07-17 21:30:04 +0100

    add test

---
+++ test/clset_test.cpp
@@ -0,0 +1,45 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file clset_test.cpp
+ * @author Richard Preen <rpreen@gmail.com>
+ * @copyright The Authors.
+ * @date 2023.
+ * @brief Set tests.
+ */
+
+#include "../lib/doctest/doctest/doctest.h"
+
+extern "C" {
+#include "../xcsf/pa.h"
+#include "../xcsf/param.h"
+#include "../xcsf/utils.h"
+#include "../xcsf/xcsf.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+}
+
+TEST_CASE("CLSET")
+{
+    struct XCSF xcsf;
+    param_init(&xcsf, 4, 1, 1);
+    rand_init_seed(2);
+    xcsf_init(&xcsf);
+    xcsf_free(&xcsf);
+    param_free(&xcsf);
+}
