48a31d3631652c67be6c2f5edb79c835da0c035b
Richard Preen <rpreen@gmail.com>
2023-08-08 19:36:12 +0100

    add support for python3.12

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Operating System :: POSIX :: Linux",
