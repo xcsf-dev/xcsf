565d36639e9f272031c5536a874d2141f010af71
Richard Preen <rpreen@gmail.com>
2024-07-28 16:10:51 +0100

    add Python seeding test

--- test/python/test_xcsf.py
+++ test/python/test_xcsf.py
@@ -22,6 +22,7 @@ from __future__ import annotations
 
 from collections import namedtuple
 
+import json
 import os
 import pickle
 import numpy as np
@@ -34,11 +35,14 @@ import xcsf
 
 SEED: int = 101
 PKL_FILENAME: str = "blah.pkl"
+POP_FILENAME: str = "pset.json"
 
 Data = namedtuple(
     "Data",
     ["x_dim", "y_dim", "x_train", "y_train", "x_val", "y_val", "x_test", "y_test"],
 )
+
+np.random.seed(SEED)
 
 
 @pytest.fixture(scope="module")
@@ -80,7 +84,26 @@ def data() -> Data:
     )
 
 
-def predictions():
+def dicts_equal(d1: dict, d2: dict) -> bool:
+    """Return whether all items in d1 are present and equal in d2.
+
+    Recursively checks if all items in the first dictionary (d1) are present and
+    equal to the corresponding items in the second dictionary (d2). This function
+    also handles nested dictionaries.
+    """
+    for key, value in d1.items():
+        if key not in d2:
+            return False
+        if isinstance(value, dict):
+            if not isinstance(d2[key], dict) or not dicts_equal(value, d2[key]):
+                return False
+        else:
+            if d2[key] != value:
+                return False
+    return True
+
+
+def predictions() -> list[dict]:
     """Return list of prediction args."""
     return [
         {"type": "constant"},
@@ -92,7 +115,7 @@ def predictions():
     ]
 
 
-def conditions():
+def conditions() -> list[dict]:
     """Return list of condition args."""
     return [
         {"type": "dummy"},
@@ -142,7 +165,7 @@ def test_serialization(data, condition, prediction):
     Note:
     -----
     Calling `predict()` will modify internal parameters such as the current
-    prediction, matching state, etc., so we have to make sure to perform
+    prediction, matching state, etc., so we have make sure to perform
     comparisons before modification.
     """
     # create model
@@ -189,3 +212,80 @@ def test_serialization(data, condition, prediction):
     # clean up
     if os.path.exists(PKL_FILENAME):
         os.remove(PKL_FILENAME)
+
+
+def test_seeding(data):
+    """Test population seeding.
+
+    Currently only tests hyperrectangle ubr.
+    """
+    # create human-designed classifier
+    classifier: dict = {
+        "error": 0.05,
+        "fitness": 0.3,
+        "set_size": 100,
+        "numerosity": 2,
+        "experience": 3,
+        "time": 3,
+        "samples_seen": 2,
+        "samples_matched": 1,
+        "condition": {
+            "type": "hyperrectangle_ubr",
+            "bound1": np.round(np.random.rand(data.x_dim), 10).tolist(),
+            "bound2": np.round(np.random.rand(data.x_dim), 10).tolist(),
+            "mutation": [0.2],
+        },
+    }
+
+    # write population set file
+    with open(POP_FILENAME, "w", encoding="utf-8") as file:
+        pset = {"classifiers": [classifier]}
+        json.dump(pset, file)
+
+    # create model with initial population set from a file
+    xcs = xcsf.XCS(
+        x_dim=data.x_dim,
+        y_dim=data.y_dim,
+        n_actions=1,
+        random_state=SEED,
+        population_file=POP_FILENAME,
+        max_trials=1,
+        pop_init=False,
+        action={"type": "integer"},
+        condition={"type": "hyperrectangle_ubr"},
+        prediction={"type": "nlms_linear"},
+    )
+
+    # add human-designed classifier again, this time manually
+    clj: str = json.dumps(classifier)  # dictionary to JSON
+    xcs.json_insert_cl(clj)
+
+    # get current population
+    pop: list[dict] = json.loads(xcs.json())["classifiers"]
+
+    # check two classifiers are present
+    assert len(pop) == 2
+
+    # check population has been seeded correctly
+    for cl in pop:
+        assert dicts_equal(classifier, cl)
+
+    # fit a single sample
+    X1 = data.x_train[0].reshape(1, -1)
+    y1 = data.y_train[0].reshape(1, -1)
+    xcs.fit(X1, y1, warm_start=True)
+
+    # get updated population
+    new_pop: list[dict] = json.loads(xcs.json())["classifiers"]
+
+    # check an additional classifier has been added via covering
+    assert len(new_pop) == 3
+
+    # check conditions are still present
+    classifier = {k: classifier[k] for k in ["condition"] if k in classifier}
+    for cl in new_pop[1:]:  # skip first (covered) classifier
+        assert dicts_equal(classifier, cl)
+
+    # clean up
+    if os.path.exists(POP_FILENAME):
+        os.remove(POP_FILENAME)
